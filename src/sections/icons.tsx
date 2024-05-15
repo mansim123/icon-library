a8aadd0aee6376629d8683ab31d2cfa0d1c643ab
Manuel yemoh <manuelyemoh@gmail.com>
2024-05-15 09:17:31 +0100

    minor fixes

--- src/sections/icons.tsx
+++ src/sections/icons.tsx
@@ -1,13 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
-// import { useAppSelector } from "@/redux/store";
-import {
-  CheckoutNumber,
-  CheckoutToggle,
-} from "@/redux/features/checkout-slice";
 import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/redux/store";
-import { useAppSelector } from "@/redux/store";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { CheckoutNumber, CheckoutToggle } from "@/redux/features/checkout-slice";
 import { useTheme } from "next-themes";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Toaster, toast } from "sonner";
@@ -58,18 +53,16 @@ interface IconData {
   };
 }
 
-
-// Define a TypeScript interface for the icon data
 interface Icon {
   name: string;
+  id: number;
   srcLight: string;
   srcDark: string;
   srcLightPng: string;
   srcDarkPng: string;
-  IconPack:string;
-  category: string;  
-  style: string; 
-  id: number;
+  iconPack: string;
+  category: string;
+  style: string;
 }
 
 export default function Icons() {
@@ -87,62 +80,53 @@ export default function Icons() {
   const [animationCode, setAnimationCode] = useState("opacity-0");
   const [svgPaths, setSvgPaths] = useState<Icon[]>([]);
 
-
-  const [iconPosts, setIconPosts] = useState([]);
-
-  // Fetch and transform icons data
-useEffect(() => {
-  const fetchIconPosts = async () => {
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/icons-mains?populate=*`);
-      const json = await res.json();
-      if (Array.isArray(json.data) && json.data.length > 0) {
-        const transformedData = json.data.map((item: IconData) => ({
-          name: item.attributes.Icon_Description,
-          srcDark: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Dark_Icon.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Dark_Icon.data.attributes.url}`,
-          srcLight: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Light_Icon.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Light_Icon.data.attributes.url}`,
-          srcDarkPng: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Dark_Icon_Png.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Dark_Icon_Png.data.attributes.url}`,
-          srcLightPng: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Light_Icon_Png.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Light_Icon_Png.data.attributes.url}`,
-          iconPack: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Icon_Pack.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Icon_Pack.data.attributes.url}`,
-          category: item.attributes.Shape,
-          style: item.attributes.Style,
-          id: item.id
-        }));
-        setIconPosts(transformedData);
-      }
-      //console.log(json.data);
-    } catch (error) {
-      console.error("Error fetching icon posts:", error);
-    }
-  };
-
-  fetchIconPosts();
-}, []);
+  const [iconPosts, setIconPosts] = useState<Icon[]>([]);
 
   useEffect(() => {
-    if (iconPosts) {
+    const fetchIconPosts = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/icons-mains?populate=*`);
+        const json = await res.json();
+        if (Array.isArray(json.data) && json.data.length > 0) {
+          const transformedData = json.data.map((item: IconData) => ({
+            name: item.attributes.Icon_Description,
+            id: item.id,
+            srcDark: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Dark_Icon.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Dark_Icon.data.attributes.url}`,
+            srcLight: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Light_Icon.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Light_Icon.data.attributes.url}`,
+            srcDarkPng: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Dark_Icon_Png.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Dark_Icon_Png.data.attributes.url}`,
+            srcLightPng: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Light_Icon_Png.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Light_Icon_Png.data.attributes.url}`,
+            iconPack: `${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.Icon_Pack.data.attributes.url.startsWith('/') ? '' : '/'}${item.attributes.Icon_Pack.data.attributes.url}`,
+            category: item.attributes.Shape,
+            style: item.attributes.Style
+          }));
+          setIconPosts(transformedData);
+        }
+      } catch (error) {
+        console.error("Error fetching icon posts:", error);
+      }
+    };
+
+    fetchIconPosts();
+  }, []);
+
+  useEffect(() => {
+    if (iconPosts.length > 0) {
       console.log(iconPosts);
     }
   }, [iconPosts]);
-  
+
   useEffect(() => {
-    // Dispatch the action after the state has been updated
     dispatch(CheckoutNumber(checkoutNumber));
   }, [checkoutNumber, dispatch]);
 
   useEffect(() => {
-    //const color = theme === "dark" ? "#ffffff" : "#000000";
-    // changeSVGColor(color);
-
     const timeoutId = setTimeout(() => {
       setAnimationCode("transition duration-200 opacity-1");
     }, 200);
 
-    // Cleanup function to clear the timeout if the component unmounts
     return () => clearTimeout(timeoutId);
-  }, [theme]); // Only run this effect when the theme changes
+  }, [theme]);
 
-  // Function to toggle selection
   function toggleSelection(
     name: string,
     id: number,
@@ -151,6 +135,8 @@ useEffect(() => {
     srcDarkPng: string,
     srcLightPng: string,
     iconPack: string,
+    category: string,
+    style: string
   ) {
     const exists = svgPaths.some((item) => item.id === id);
 
@@ -170,16 +156,13 @@ useEffect(() => {
       return;
     }
 
-    // Update svgPaths state by adding the new item
     setSvgPaths((prevState) => [
       ...prevState,
-      { name, id, srcLight, srcDark, srcDarkPng, srcLightPng, iconPack },
+      { name, id, srcLight, srcDark, srcDarkPng, srcLightPng, iconPack, category, style }
     ]);
 
-    // Increment checkoutNumber
     setCheckoutNumber((prevCheckoutNumber) => prevCheckoutNumber + 1);
 
-    // Run toast function
     toast("Icon has been added to basket", {
       description: "Go to checkout to view all items",
       action: {
@@ -192,10 +175,9 @@ useEffect(() => {
   }
 
   const removeItemFromSvgPaths = (idToRemove: number) => {
-    // Check if the item with the given id exists in svgPaths
     const indexToRemove = svgPaths.findIndex((item) => item.id === idToRemove);
 
-    toast("Icon has been remove from your basket", {
+    toast("Icon has been removed from your basket", {
       description: "Go to checkout to view all items",
       action: {
         label: "Checkout",
@@ -205,51 +187,41 @@ useEffect(() => {
       },
     });
 
-    // If the item is found, remove it from svgPaths
     if (indexToRemove !== -1) {
       setSvgPaths((prevState) => {
         const newState = [...prevState];
-        newState.splice(indexToRemove, 1); // Remove the item at the found index
+        newState.splice(indexToRemove, 1);
         return newState;
       });
 
-      // Decrement checkoutNumber
       setCheckoutNumber((prevCheckoutNumber) => prevCheckoutNumber - 1);
     }
   };
-
-  // const checkoutToggleField = useAppSelector(
-  //   (state) => state.checkOutSlice.value.checkoutToggleField
-  // );
 
   const toggleCheckout = () => {
     dispatch(CheckoutToggle());
   };
 
-  const [filteredIcons, setFilteredIcons] = useState([]);
+  const [filteredIcons, setFilteredIcons] = useState<Icon[]>([]);
 
-  // Filter icons based on category when category changes
-  // Filter icons based on category, shape, and searchName
   useEffect(() => {
     const filtered = iconPosts.filter((icon) => {
       const matchesCategory = category === "all" || icon.category === category;
-      const matchesShape = shape === "all" || icon.style === shape; // Use 'style' property to match 'shape'
+      const matchesShape = shape === "all" || icon.style === shape;
       const matchesSearch = icon.name.toLowerCase().includes(searchName.toLowerCase());
-  
+
       return matchesCategory && matchesShape && matchesSearch;
     });
-  
+
     setFilteredIcons(filtered);
   }, [iconPosts, category, shape, searchName]);
 
   const onCategoryChange = (value: string) => {
     setCategory(value);
-    // console.log(value);
   };
 
   const onShapeChange = (value: string) => {
     setShape(value);
-    // console.log(value);
   };
 
   return (
@@ -292,7 +264,7 @@ useEffect(() => {
         <div
           className={`grid px-6 grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-4 ${animationCode}`}
         >
-          {filteredIcons.map((icon:any, index:any) => (
+          {filteredIcons.map((icon, index) => (
             <button
               key={index}
               className="p-4 bg-secondary rounded-xl border border-secondary flex flex-col justify-center items-center hover:inner-border-2 focus:inner-border-2"
@@ -305,13 +277,13 @@ useEffect(() => {
                   icon.srcLightPng,
                   icon.srcDarkPng,
                   icon.iconPack,
-                  
+                  icon.category,
+                  icon.style
                 )
               }
               style={{ minWidth: "8rem", minHeight: "8rem" }}
             >
               <div className="w-[75px] h-[75px]">
-                {/* <div dangerouslySetInnerHTML={{ __html: icon.svg }} /> */}
                 <Image
                   className="inline-block dark:hidden"
                   src={icon.srcDark}
@@ -327,7 +299,6 @@ useEffect(() => {
                   alt={icon.name}
                 />
               </div>
-              {/* <Image src={icon.src} width={75} height={75} alt={icon.name} /> */}
               <small className="text-center">{icon.name}</small>
             </button>
           ))}
