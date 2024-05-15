b55732482eefb798fb95a0d6ac6e5fde333e5610
Manuel yemoh <manuelyemoh@gmail.com>
2024-05-15 09:56:24 +0100

    added env code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [process.env.IMAGE_DOMAIN], // Use the environment variable here
+    },
+  };
+  
+  export default nextConfig;
